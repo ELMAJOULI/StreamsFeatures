ba32e7530583369e4f28d1f75178550a189fcccb
Abdelwahed <abdelwahedkm@gmail.com>
2020-04-19 17:07:17 +0100

    init

---
+++ src/ma/elmajouli/stream/model/Sales.java
@@ -0,0 +1,11 @@
+package ma.elmajouli.stream.model;
+
+import java.util.Date;
+
+public class Sales {
+    private String company;
+    private String casablanca;
+    private Date saleYear;
+    private Double price;
+
+}
